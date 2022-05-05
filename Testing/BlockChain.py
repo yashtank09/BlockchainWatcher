827e58891c21d61661b18e2dbb9586b2d93e858a
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-05-05 20:01:23 +0530

    Success to install web3 on Ubuntu, still have that windows installation error

--- Testing/BlockChain.py
+++ Testing/BlockChain.py
@@ -1,2 +1,19 @@
+# web3 Module for interacting with Etherium
+from web3 import Web3
 
-import datetime
+# Getting Infura url that allow developers to easily take their blockchain application from testing to scaled deployment - with simple, reliable access to Ethereum and IPFS
+Infura_URL = "https://mainnet.infura.io/v3/78371715113f4bdd946518414b9de62e"
+
+# make connection to Blockchain
+web3 = Web3(Web3.HTTPProvider(Infura_URL))
+
+# Checking is we connected with Chain
+print(web3.isConnected())
+
+# here we are printing block number
+print(web3.eth.blockNumber)
+
+# getting balance of Metamask Wallet using their hash code
+Balance = web3.eth.getBalance('0xfEa5D9023Cd95F3a60584C812f3d5C8cEDa41a7d')
+
+print(web3.fromWei(Balance, 'ether'))
